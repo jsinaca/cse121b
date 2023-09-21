12f4a9d374641797341b3103167e8adeb8cb3b3f
JosueSinaca <sinaca56@gmail.com>
2023-09-20 21:48:10 -0600

    w02

---
+++ opandcomp.js
@@ -0,0 +1,23 @@
+let a= 1;
+let b = 2;
+
+console.log(a+b);
+console.log(a*b);
+console.log(a-b);
+
+console.log("hello"+"world");
+
+let c = 2;
+c+= 3;
+
+a == b; 
+if (4 < 5) {
+
+}
+if (4 != 5 ) {}
+
+if (a < 10 && a > 0) {}
+
+
+
+
---
+++ scripts/w02-task.js
@@ -0,0 +1,33 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Josue Sinaca Gomez";
+let currentYear = "2023";
+let profilePicture = "/images/profile.JPG";
+
+/* Step 3 - Element Variables */
+let nameElement = document.getElementById("name");
+let foodElement = document.getElementById("food");
+let yearElement = document.querySelector("#year");
+let imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+
+
+/* Step 5 - Array */
+let favoriteFood = ["Asadito", "Torta de Pina", "Tacos de asada", "Tacos al pastor", "Tacos de ojo", "Tacos de tripa", "Arros chaufa", "Fried fish", "Suchi", "Steak", "Enchiladas", "Rotisserie chicken", "Paella", "Saltenas", "Charque"];
+foodElement.innerHTML = favoriteFood;
+let singleFood = "Tacos de suadero";
+favoriteFood.push(singleFood);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood}`;
